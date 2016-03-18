28c238e549ebaa2de3ab18a99e7332fec0fd8055
Lucas <lucas@agtlucas.com>
2016-03-18 09:15:20 -0300

    Update webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry: [
+    'webpack-hot-middleware/client?reload=true'
+  ]
+}
