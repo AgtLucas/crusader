2c8a50f6c8d12b1f673b9dafef4e1849c5db5d42
Lucas <lucas@agtlucas.com>
2016-03-20 20:51:09 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
 
+class App extends Component {
+  render () {
+    return (
+      <div>Foo!</div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app__root'))
